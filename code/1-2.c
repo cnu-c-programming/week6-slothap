f805e92a5a3f5ce31fe279589649fb39e466e02d
slothap <ueun6906@gmail.com>
2026-04-10 17:04:30 +0900

    과제제출 1

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    
+    int arr2[2][4] = {{1,2,3,4}, {5,6,7,8}};
+
+    int (*p)[4];
+
+    for(int i = 0; i <2 ; i++){
+        p = arr2 + i; //int[4] -> 4*i만큼
+        for(int j = 0; j < 4; j++){
+            printf("%d\n", *(*p + j)); //p -> [0]/[4]->(+j: *p는 int) -> +j만큼
+        }
+    }
+    // 바깥쪽 루프 - p = arr2 + i;
+    // int[4] -> + 4*i만큼
+
+    // 안쪽 루프- *(*p + j)
+    // p -> [0]/[4] -> (*p는 int 시작 주소) -> +j 만큼
 
     return 0;
 }
--- code/1-3.c
+++ code/1-3.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    
+    char *names[5] = {"kim", "lee", "park", "choi", "jeong"};
 
+    for(int i = 0; i<5; i++){
+        for (int j = 0; ;j++){
+            char c = *(*(names + i) + j);
+            if(c == '\0')
+                break;
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+
+    // name                 (char **), 8(포인터 크기)
+    // name + i             -> 8*i
+    // *(name + i)          문자열의 시작 주소(char *), 1(글자 크기)
+    // *(name + i) + j      -> 1*j
+    // *(*(name + i) + j)   이동한 위치의 데이터를 c로 함
     return 0;
 }
